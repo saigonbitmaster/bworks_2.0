d574f3a59faa1b1471007b0f3d7968049e4e61de
saigonbitmaster <bitmaster.sg@gmail.com>
2022-10-20 20:20:08 +0700

    user own data

---
+++ api/src/flatworks/types/types.ts
@@ -0,0 +1,44 @@
+export interface GitCommit {
+  id: string;
+  message: string;
+  url: string;
+  date: Date;
+}
+
+export interface FundTransaction {
+  hash: string;
+  amount: number;
+  date: Date;
+}
+
+export interface RaList {
+  count: number;
+  data: any[];
+}
+
+export interface AddressUtxoType {
+  tx_hash: string;
+  block: string;
+  amount: { unit: string; quantity: number }[];
+}
+
+export type ProjectStatus = 'pending' | 'complete' | 'stopped';
+export interface userJwtPayload {
+  userId: string;
+  username: string;
+}
+
+export interface MongooseQuery {
+  filter: { [key: string]: any };
+  sort: { [key: string]: any };
+  skip: number;
+  limit: number;
+}
+
+export interface CheckWalletType {
+  amount: number;
+  enough: boolean;
+}
+export interface GitLink {
+  gitUrl: string;
+}
---
+++ api/src/flatworks/utils/filter.scammer.ts
@@ -0,0 +1,3 @@
+const ScamFilter = (userId: string) => {};
+
+export {ScamFilter}
