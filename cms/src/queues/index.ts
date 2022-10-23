c0386726e487541867888fd546ad338b5c009107
saigonbitmaster <bitmaster.sg@gmail.com>
2022-10-23 13:45:55 +0700

    queue job admin UIs

--- cms/src/queues/index.ts
+++ cms/src/queues/index.ts
@@ -1,13 +1,9 @@
 import Icon from "@mui/icons-material/CurrencyExchange";
-import ListScreen from "./list";
-import CreateScreen from "./create";
-import EditScreen from "./edit";
+import ListScreen from "./queueList";
 
 const index = {
   list: ListScreen,
   icon: Icon,
-  create: CreateScreen,
-  edit: EditScreen,
 };
 
 export default index;
