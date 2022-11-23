965c642cf4f2e61c9fe5039d82145f4586b9a1f2
saigonbitmaster <bitmaster.sg@gmail.com>
2022-11-24 01:56:35 +0700

    task management frontend

---
+++ jsk/src/jobtasks/create.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+  NumberInput,
+  DateTimeInput,
+} from "react-admin";
+import Grid from "@mui/material/Grid";
+import { RichTextInput } from "ra-input-rich-text";
+import { taskStatus } from "../types";
+
+const statusChoices: any[] = [
+  { id: "inProgress", name: "In progress" },
+  { id: "todo", name: "Todo" },
+  { id: "completed", name: "Completed" },
+];
+
+const CreateScreen = () => (
+  <Create redirect="list">
+    <SimpleForm>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6} lg={5} xl={3}>
+          <ReferenceInput source="jobId" reference="postjobs">
+            <SelectInput optionText="name" fullWidth />
+          </ReferenceInput>
+        </Grid>
+        <Grid item md={12} />
+        <Grid item xs={12} md={6} lg={5} xl={3}>
+          <TextInput source="name" fullWidth required />
+        </Grid>
+
+        <Grid item xs={12} md={4} lg={3} xl={2}>
+          <SelectInput source="status" choices={statusChoices} />
+        </Grid>
+        <Grid item md={12} />
+        <Grid item xs={12} md={6} lg={5} xl={3}>
+          <DateTimeInput
+            source="startDate"
+            fullWidth
+            defaultValue={new Date()}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={5} xl={3}>
+          <DateTimeInput source="deadline" fullWidth />
+        </Grid>
+        <Grid item md={12} />
+        <Grid item xs={12} lg={10} xl={6}>
+          <TextInput source="gitLink" fullWidth  />
+        </Grid>
+        <Grid item md={12} />
+        <Grid item xs={12} md={12} lg={10} xl={6}>
+          <RichTextInput source="description" fullWidth />
+        </Grid>
+      </Grid>
+    </SimpleForm>
+  </Create>
+);
+export default CreateScreen;
---
+++ jsk/src/jobtasks/list.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  EditButton,
+  DateField,
+  SelectField,
+  ReferenceField,
+  ReferenceInput,
+  SelectInput,
+} from "react-admin";
+const statusChoices: any[] = [
+  { id: "inProgress", name: "In progress" },
+  { id: "todo", name: "Todo" },
+  { id: "completed", name: "Completed" },
+];
+
+const jobFilters = [
+  <ReferenceInput source="jobId" reference="postjobs" alwaysOn>
+    <SelectInput optionText="name" fullWidth />
+  </ReferenceInput>,
+  <SelectInput source="status" choices={statusChoices} alwaysOn />,
+];
+
+const ListScreen = () => {
+  return (
+    <List
+      perPage={25}
+      sort={{ field: "jobId", order: "desc" }}
+      hasCreate
+      resource="jobtasks"
+      filter={{ queryType: "jobSeeker" }}
+      filters={jobFilters}
+    >
+      <Datagrid>
+        <TextField source="name" />
+        <ReferenceField reference="postJobs" source="jobId">
+          <TextField source="name" />
+        </ReferenceField>
+        <ReferenceField reference="users" source="creator">
+          <TextField source="fullName" />
+        </ReferenceField>
+        <ReferenceField reference="users" source="updater">
+          <TextField source="fullName" />
+        </ReferenceField>
+        <DateField source="startDate" showTime />
+        <DateField source="deadline" showTime />
+        <SelectField source="status" choices={statusChoices} />
+        <EditButton />
+      </Datagrid>
+    </List>
+  );
+};
+
+export default ListScreen;
